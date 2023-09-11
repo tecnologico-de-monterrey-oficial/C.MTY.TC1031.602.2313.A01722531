394e02f94fcc7d989ed8f01710cc1968c856d277
gretaguagnelli <136791332+gretaguagnelli@users.noreply.github.com>
2023-09-11 00:57:28 -0600

    Update act4.cpp

--- Assignments/Act1.4-SearchAlgorithms/act4.cpp
+++ Assignments/Act1.4-SearchAlgorithms/act4.cpp
@@ -1,3 +1,7 @@
+// greta guagnelli
+// a01722531
+// act 1.4
+
 #include <iostream>
 #include <vector>
 #include <chrono>
@@ -32,23 +36,22 @@ int binarySearch(vector<int> list, int data) {
             }
         }
     }
-    // Se termina el ciclo, no se encontró el valor
-    // throw out_of_range('No se encontró el valor buscado')
+    // se termina el ciclo no se encontró el valor
     return -1;
 }
 
-// Obtiene el tiempo inicial para calcular el tiempo transcurrido por un algoritmo
+// obtener  tiempo inicial para calcular tiempo transcurrido por un algoritmo:
 void startTime(chrono::high_resolution_clock::time_point &begin)
 {
   // start time
-  begin = std::chrono::high_resolution_clock::now();
+  begin = chrono::high_resolution_clock::now();
   
 }
 
-// Imprime el tiempo transcurrido desde el valor de start hasta el momento que se ejecuta la función
+// imprimir tiempo transcurrido desde el valor de start hasta el momento que se ejecuta la función:
 void getTime(chrono::high_resolution_clock::time_point begin, chrono::high_resolution_clock::time_point end) 
 {
-    end = std::chrono::high_resolution_clock::now();
+    end = chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
 
     printf("Tiempo de ejecución: %.8f seconds.\n", elapsed.count() * 1e-9);
@@ -73,18 +76,10 @@ int main() {
     chrono::high_resolution_clock::time_point begin;
     chrono::high_resolution_clock::time_point end;
 
-    // vector<int> list = {5,12,23,27,32,34,38,40,45};
-    // int n = 15;
-    // int index = binarySearch(list,n);
-    // if (index >= 0) {
-    //     cout << "El valor " << n << " se encuentra en la posición " << index << endl;
-    // } else {
-    //     cout << "El valor " << n << " no se encuentra en la lista" << endl;
-    // }
+
 
     vector<int>
 
-return 0;
+    return 0;
 
 }
-
